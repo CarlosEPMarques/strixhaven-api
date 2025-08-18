76b28e37896abd887dde1cec059d589e88ac99b6
Carlos Marques <carlos.marques@bhz.jamef.com.br>
2025-08-18 16:39:03 -0300

    fix: Add input_data parameter to update_book route

--- src/modules/book/book_router.py
+++ src/modules/book/book_router.py
@@ -4,7 +4,7 @@ from fastapi.param_functions import Depends
 from starlette import status
 
 from src.modules.book.book_injection import book_usecase_factory
-from src.modules.book.book_schema import BookCreateInput, BookOutput
+from src.modules.book.book_schema import BookCreateInput, BookOutput, BookUpdateInput
 from src.modules.book.book_usecase import BookUseCase
 
 router = APIRouter(prefix='/books', tags=['Books'])
@@ -22,8 +22,8 @@ async def get_book(book_id: str, usecase: Annotated[BookUseCase, Depends(book_us
     return await usecase.find_book(book_id=book_id)
 
 @router.put('/{book_id}', status_code=status.HTTP_202_ACCEPTED)
-async def update_book(book_id: str, usecase: Annotated[BookUseCase, Depends(book_usecase_factory)]) -> BookOutput:
-    return await usecase.update_book(book_id=book_id)
+async def update_book(book_id: str, input_data: BookUpdateInput, usecase: Annotated[BookUseCase, Depends(book_usecase_factory)]) -> BookOutput:
+    return await usecase.update_book(book_id=book_id, input_data=input_data)
 
 @router.delete('/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: str, usecase: Annotated[BookUseCase, Depends(book_usecase_factory)]) -> None:
